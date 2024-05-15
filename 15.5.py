f6b98f6f0326106d28e4a5ca8b5a7653998d2cfe
claricewt <160562287+claricewt@users.noreply.github.com>
2024-05-15 10:14:53 -0300

    Add files via upload

---
+++ 15.5.py
@@ -0,0 +1,37 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============================================================= RESTART: C:/Users/202102531055/Desktop/15.5 2.py =============================================================
+dinheiro
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    dinheiro
+NameError: name 'dinheiro' is not defined
+def numeros_pares(numero)
+SyntaxError: expected ':'
+return list(filter(lambada x: x % 2 == 0, range(1, numero +1)))
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+numero = 10
+print(numeros_pares(numero)) #
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    print(numeros_pares(numero)) #
+NameError: name 'numeros_pares' is not defined
+
+============================================================= RESTART: C:/Users/202102531055/Desktop/15.5 2.py =============================================================
+[2, 4, 6, 8, 10]
+range(3)
+range(0, 3)
+list(range(2,5,2))
+[2, 4]
+list(range(5,2,-2))
+[5, 3]
+lista(range(2,5,2))
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    lista(range(2,5,2))
+  File "C:/Users/202102531055/Desktop/15.5 2.py", line 25, in lista
+    return list(range(2, n+1, 2))
+TypeError: unsupported operand type(s) for +: 'range' and 'int'
+list(range(2,5,2))
+[2, 4]
